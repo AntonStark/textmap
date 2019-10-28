eab06babafdf6227e108124bd69130b6a00697f8
anton yakovlev <Lantar23@yandex.ru>
2019-10-28 16:28:56 +0300

    setup db

--- textmap/textmap/settings.py
+++ textmap/textmap/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'textmap.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'textmap',
+        'USER': 'textmap',
+        'PASSWORD': 'sn3',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
