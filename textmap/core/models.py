ac1e7bdc7b373478c458a4165479e48524496b09
anton yakovlev <Lantar23@yandex.ru>
2019-11-02 14:03:43 +0300

    create models

--- textmap/core/models.py
+++ textmap/core/models.py
@@ -1,3 +1,47 @@
+import uuid
+from django.contrib.postgres import fields
 from django.db import models
 
-# Create your models here.
+
+class Text(models.Model):
+    """
+    Main object ro store information about individual text
+    """
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    root_uid = models.OneToOneField('Part', related_name='+',
+                                    on_delete=models.CASCADE, editable=False)
+
+    name = models.TextField(blank=False, null=False)
+    authors = fields.JSONField(default=dict)
+    language = models.CharField(max_length=8, default='ru')
+
+
+class Part(models.Model):
+    """
+    Store arbitrary tree structure
+    """
+    id = models.BigAutoField(primary_key=True)
+    text = models.ForeignKey(Text, on_delete=models.CASCADE)
+    parent_id = models.ForeignKey('self', on_delete=models.CASCADE)
+
+    subtitle = models.TextField(blank=True, null=False, default='')
+
+
+class Paragraph(models.Model):
+    """
+    Smallest part of text as a bag of ideas.
+    """
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    part = models.ForeignKey(Part, on_delete=models.CASCADE)
+    prev = models.OneToOneField('self', related_name='+',
+                                on_delete=models.SET_NULL, null=True, default=None)
+    next = models.OneToOneField('self', related_name='+',
+                                on_delete=models.SET_NULL, null=True, default=None)
+    sentence_ids = fields.ArrayField(models.BigIntegerField())
+
+
+class Sentence(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    paragraph = models.ForeignKey(Paragraph, on_delete=models.CASCADE)
+
+    raw = models.TextField()
